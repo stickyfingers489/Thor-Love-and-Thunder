d1f6a1425f0c9919874e427964ea07f039420fc6
stickyfingers489 <86178881+stickyfingers489@users.noreply.github.com>
2022-08-30 05:17:24 +0300

    Update Thor Love and Thunder/Cells/CoursesTableViewCell.swift
    Co-authored-by: Sylvan Ash <4120488+sylvan-d-ash@users.noreply.github.com>

--- Thor Love and Thunder/Cells/CoursesTableViewCell.swift
+++ Thor Love and Thunder/Cells/CoursesTableViewCell.swift
@@ -39,11 +39,4 @@ class CoursesTableViewCell: UITableViewCell {
         subtitleLabel.maximumContentSizeCategory = .extraLarge
         
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-     
-    }
-
 }
